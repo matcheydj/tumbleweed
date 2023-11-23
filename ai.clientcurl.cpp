8ccefd889a6d9a7e670a864766d81845906e5223
matcheydj <matcheydj@users.noreply.github.com>
2023-11-23 17:29:03 -0600

    Create ai.clientcurl.cpp

---
+++ ai.clientcurl.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <curl/curl.h>
+
+int main() {
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5000/api/method");
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+    return 0;
+}
